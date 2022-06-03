86a6e2295e0de6cc22a448e0884b21d235d4feb8
verah <verahnyagaka101@gmail.com>
2022-06-03 22:37:42 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+// // Your code here
+       
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    dodger.style.backgroundColor = "#FF69B4";
+    dodger.style.bottom = "0px";
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerLeft();
+        }
+      });
+  }
+  moveDodgerLeft();
+
+// const dodger = document.getElementById("dodger");
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
